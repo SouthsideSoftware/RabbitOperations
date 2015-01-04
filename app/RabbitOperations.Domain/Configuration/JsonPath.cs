389f1ee98a4e61ef9f20db8b575493902eb6f70e
Tom Cabanski <tom@cabanski.com>
2015-01-04 10:33:16 -0600

    adding some missing files

---
+++ app/RabbitOperations.Domain/Configuration/JsonPath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SouthsideUtility.Core.DesignByContract;
+
+namespace RabbitOperations.Domain.Configuration
+{
+    public class JsonPath
+    {
+        protected JsonPath()
+        {
+            Parts = new List<string>();
+        }
+        public JsonPath(string path)
+        {
+            Verify.RequireNotNull(path, "path");
+
+            Parts = new List<string>(path.Split('.'));
+            
+        }
+        public IList<string> Parts { get; set; }
+        public override string ToString()
+        {
+            return string.Join(".", Parts);
+        }
+    }
+}
