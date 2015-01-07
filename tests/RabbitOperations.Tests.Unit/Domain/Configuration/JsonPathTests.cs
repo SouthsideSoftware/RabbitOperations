cb94a7378a5480c3a39bc62580fea6d07d0b12a4
Tom Cabanski <tom@cabanski.com>
2015-01-07 05:03:17 -0600

    Missed a change

--- tests/RabbitOperations.Tests.Unit/Domain/Configuration/JsonPathTests.cs
+++ tests/RabbitOperations.Tests.Unit/Domain/Configuration/JsonPathTests.cs
@@ -52,5 +52,25 @@ namespace RabbitOperations.Tests.Unit.Domain.Configuration
             //assert
             path.Parts.Should().Equal(new List<string> { "Item1", "Item2"});
         }
+
+        [Test]
+        public void ShoulTrimParts()
+        {
+            //act
+            var path = new JsonPath("  Item1 .\tItem2.");
+
+            //assert
+            path.Parts.Should().Equal(new List<string> { "Item1", "Item2" });
+        }
+
+        [Test]
+        public void ShouldReturnDottedPathFromToString()
+        {
+            //act
+            var path = new JsonPath("Item1.Item2.Item3");
+
+            //assert
+            path.ToString().Should().Be("Item1.Item2.Item3");
+        }
     }
 }
