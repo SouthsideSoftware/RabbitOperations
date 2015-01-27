2bb1b23f652b657b85ff7d9f7d2fe412a6e71d19
Tom Cabanski <tom@cabanski.com>
2015-01-27 13:26:42 -0600

    Improved footer layout

---
+++ app/RabbitOperations.Collector/Web/VersionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SouthsideUtility.Core.DesignByContract;
+
+namespace RabbitOperations.Collector.Web
+{
+    [ExcludeFromCodeCoverage] //just a thin wrapper around FileVersionInfo
+    public static class VersionHelper
+    {
+        private static readonly string version = FileVersionInfo.GetVersionInfo(typeof(VersionHelper).Assembly.Location).ProductVersion;
+        public static string Version()
+        {
+            return version;
+        }
+    }
+
+}
