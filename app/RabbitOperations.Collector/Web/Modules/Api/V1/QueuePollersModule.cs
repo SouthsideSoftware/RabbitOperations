12c10e5f7e69d817798c601094837ac0df202629
Tom Cabanski <tom@cabanski.com>
2015-02-13 10:39:35 -0600

    Queue display comes from ajax call instead of signalr

--- app/RabbitOperations.Collector/Web/Modules/Api/V1/QueuePollersModule.cs
+++ app/RabbitOperations.Collector/Web/Modules/Api/V1/QueuePollersModule.cs
@@ -1,17 +1,26 @@
 using Nancy;
+using RabbitOperations.Collector.Configuration.Interfaces;
+using RabbitOperations.Collector.Service.Interfaces;
+using SouthsideUtility.Core.DesignByContract;
 
 namespace RabbitOperations.Collector.Web.Modules.Api.V1
 {
     public class QueuePollersModule : NancyModule
     {
-        public QueuePollersModule() : base("Api/V1/QueuePollers")
+        private readonly IActiveQueuePollers activeQueuePollers;
+
+        public QueuePollersModule(IActiveQueuePollers activeQueuePollers) : base("Api/V1/QueuePollers")
         {
+            this.activeQueuePollers = activeQueuePollers;
+            Verify.RequireNotNull(activeQueuePollers, "activeQueuePollers");
+
+            this.activeQueuePollers = activeQueuePollers;
+            
             Get["/"] = parameters =>
             {
 
-                return "stuff for v1";
+                return activeQueuePollers;
             };
         }
-
     }
 }
