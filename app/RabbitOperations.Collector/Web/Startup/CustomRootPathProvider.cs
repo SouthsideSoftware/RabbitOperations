d30bb1a979db808415d4a1dd34b2a3638e431b8b
Tom Cabanski <tom@cabanski.com>
2015-02-14 08:26:31 -0600

    Supports in place development of client-side web components when Web directory is present two levels below the executable (e.g. on a developer box)

--- app/RabbitOperations.Collector/Web/Startup/CustomRootPathProvider.cs
+++ app/RabbitOperations.Collector/Web/Startup/CustomRootPathProvider.cs
@@ -6,7 +6,18 @@ namespace RabbitOperations.Collector.Web.Startup
 {
     public class CustomRootPathProvider : IRootPathProvider
     {
-        private static readonly string appLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Web");
+        private readonly string appLocation;
+
+        public CustomRootPathProvider()
+        {
+            //Web root points at web directory in project if source code is present otherwise
+            //points at the web folder in the same location as the executable
+            var binDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var devFileDirectory = Path.Combine(binDirectory, "..\\..\\Web");
+            var devFile = Path.Combine(devFileDirectory, "Views\\layout.cshtml");
+            appLocation = File.Exists(devFile) ? Path.GetFullPath(devFileDirectory) : Path.Combine(binDirectory, "Web");
+        }
+
         public string GetRootPath()
         {
             return appLocation;
