d9631591b6e03f0cc5b77ccc5d39a71186037d84
Tom Cabanski <tom@cabanski.com>
2015-03-09 18:42:37 -0500

    Finish up tests for the qualified schema updaters factory

--- tests/RabbitOperations.Collector.Tests.Unit/RavenDB/QualifiedSchemaUpdatersFactoryTests.cs
+++ tests/RabbitOperations.Collector.Tests.Unit/RavenDB/QualifiedSchemaUpdatersFactoryTests.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
@@ -27,6 +24,46 @@ namespace RabbitOperations.Collector.Tests.Unit.RavenDB
 
             //assert 
             updaters.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void ShouldReturnAllUpdatersWithVersionGreaterThanCurrent()
+        {
+            //arrange
+            var settingsStub = Mock.Of<ISettings>(m => m.DatabaseSchemaVersion == 2);
+            var factory = new QualifiedSchemaUpdatersFactory(new List<IUpdateSchemaVersion>
+            {
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 1),
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 2),
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 3),
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 4)
+            }, settingsStub);
+
+            //act
+            var updaters = factory.Get();
+
+            //assert 
+            updaters.Select(x => x.SchemaVersion).Should().BeEquivalentTo(new List<int> {3, 4});
+        }
+
+        [Test]
+        public void ShouldReturnQualifiedUpdatersSortedByVersionNumber()
+        {
+            //arrange
+            var settingsStub = Mock.Of<ISettings>(m => m.DatabaseSchemaVersion == 1);
+            var factory = new QualifiedSchemaUpdatersFactory(new List<IUpdateSchemaVersion>
+            {
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 4),
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 2),
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 3),
+                Mock.Of<IUpdateSchemaVersion>(m => m.SchemaVersion == 1)
+            }, settingsStub);
+
+            //act
+            var updaters = factory.Get();
+
+            //assert 
+            updaters.Select(x => x.SchemaVersion).Should().Equal(new List<int> { 2, 3, 4 });
         }
     }
 }
