51ff9b8203e9a9f46403e587160c62653a518bea
Tom Cabanski <tom@cabanski.com>
2015-04-03 11:43:27 -0500

    Time sent in header uses local time. Format durations reasonably

--- app/RabbitOperations.Collector/Web/Content/App/Dashboard/searchController.js
+++ app/RabbitOperations.Collector/Web/Content/App/Dashboard/searchController.js
@@ -34,10 +34,22 @@
 });
 
 rabbitOperationsApp.controller('searchDetailController', function($scope, $modalInstance, item) {
+    $scope.toDisplayDuration = function (duration) {
+        if (duration !== undefined) {
+            var hours = duration.days * 24 + duration.hours;
+            return hours + ":" + duration.minutes + ":" + duration.seconds + "." + duration.milliseconds;
+        } else {
+            return "UNK";
+        }
+    }
+
     $scope.message = {
         item: item,
         body: item !== undefined && item.body !== undefined ? JSON.stringify(JSON.parse(item.body), null, 2) : '',
         headers: item !== undefined && item.headers !== undefined ? JSON.stringify(item.headers, null, 2) : '',
+        timeSent: item !== undefined && item.timeSent !== undefined ? moment(item.timeSent).format('MM/DD/YYYY HH:mm:ss') : '',
+        processingTime: item !== undefined ? $scope.toDisplayDuration(item.processingTime) : 'UNK',
+        totalTime: item !== undefined ? $scope.toDisplayDuration(item.totalTime) : 'UNK'
     };
 
     $scope.ok = function () {
