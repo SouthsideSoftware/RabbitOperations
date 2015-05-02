af51ee05f281d13b65b90bd4d27edfbab0137d62
Tom Cabanski <tom@cabanski.com>
2015-05-02 10:16:32 -0500

    Fix bug: Cannot display messages where body is not json

--- app/RabbitOperations.Collector/Web/Content/App/Dashboard/searchDetailController.js
+++ app/RabbitOperations.Collector/Web/Content/App/Dashboard/searchDetailController.js
@@ -1,16 +1,25 @@
 ﻿rabbitOperationsApp.controller('searchDetailController', function($scope, $modalInstance, item, $http) {
-    $scope.toDisplayDuration = function (duration) {
+    $scope.toDisplayDuration = function(duration) {
         if (duration !== undefined) {
             var hours = duration.days * 24 + duration.hours;
             return hours + ":" + duration.minutes + ":" + duration.seconds + "." + duration.milliseconds;
         } else {
             return "UNK";
         }
-    }
+    };
+
+    $scope.parseBody = function (body) {
+        var bodyToReturn;
+        try {
+            return JSON.parse(body);
+        } catch (err) {
+            return body;
+        }
+    };
 
     $scope.message = {
         item: item,
-        body: item !== undefined && item.body !== undefined ? JSON.stringify(JSON.parse(item.body), null, 2) : '',
+        body: item !== undefined && item.body !== undefined ? JSON.stringify($scope.parseBody(item.body), null, 2) : '',
         headers: item !== undefined && item.headers !== undefined ? JSON.stringify(item.headers, null, 2) : '',
         timeSent: item !== undefined && item.timeSent !== undefined ? moment(item.timeSent).format('MM/DD/YYYY HH:mm:ss') : '',
         processingTime: item !== undefined ? $scope.toDisplayDuration(item.processingTime) : 'UNK',
