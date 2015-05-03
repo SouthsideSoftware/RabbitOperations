1a3465973d5eb2211fa80ae850bbc21eb7926533
Tom Cabanski <tom@cabanski.com>
2015-05-03 08:45:48 -0500

    Implement the basic properties service for nservicebus with tests

--- app/RabbitOperations.Collector/MessageRetry/NServiceBus/CreateBasicPropertiesService.cs
+++ app/RabbitOperations.Collector/MessageRetry/NServiceBus/CreateBasicPropertiesService.cs
@@ -1,18 +1,50 @@
 ﻿using System;
+using System.Linq;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
 using RabbitOperations.Collector.MessageParser.Interfaces;
 using RabbitOperations.Collector.MessageRetry.Interfaces;
+using RabbitOperations.Domain;
 
 namespace RabbitOperations.Collector.MessageRetry.NServiceBus
 {
     public class CreateBasicPropertiesService : ICreateBasicProperties
     {
+        private const byte persistentDelivery = 2;
         public IBasicProperties Create(IRawMessage rawMessage)
         {
-            //todo: get values from NServiceBus headers
-            //messageId, correlationId, type, contentType
-            //set DeliveryMode = 2 (persistent)
-            throw new NotImplementedException();
+            var basicProperties = new BasicProperties();
+            basicProperties.ContentType = GetHeaderValueIfExists(rawMessage, "NServiceBus.ContentType");
+            basicProperties.MessageId = GetHeaderValueIfExists(rawMessage, "NServiceBus.MessageId");
+            basicProperties.CorrelationId = GetHeaderValueIfExists(rawMessage, "NServiceBus.CorrelationId");
+            SetTypeIfHeaderExists(rawMessage, basicProperties);
+            basicProperties.DeliveryMode = persistentDelivery;
+
+            return basicProperties;
+        }
+
+        private void SetTypeIfHeaderExists(IRawMessage rawMessage, BasicProperties basicProperties)
+        {
+            string types = GetHeaderValueIfExists(rawMessage, "NServiceBus.EnclosedMessageTypes");
+            if (types != null)
+            {
+                var typeList = types.Split(';').Select(pt => { return new TypeName(pt); }).ToList();
+                if (typeList.Count > 0)
+                {
+                    basicProperties.Type = string.Format("{0}.{1}", typeList[0].Namespace, typeList[0].ClassName);
+                }
+            }
+        }
+
+        private string GetHeaderValueIfExists(IRawMessage rawMessage, string key)
+        {
+            string val = null;
+            if (rawMessage.Headers.ContainsKey(key))
+            {
+                val = rawMessage.Headers[key];
+            }
+
+            return val;
         }
     }
 }
---
+++ tests/RabbitOperations.Collector.Tests.Unit/MessageRetry/NServiceBus/CreateBasicPropertiesServiceTests.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RabbitOperations.Collector.MessageParser;
+using RabbitOperations.Collector.MessageRetry.NServiceBus;
+
+namespace RabbitOperations.Collector.Tests.Unit.MessageRetry.NServiceBus
+{
+    public class CreateBasicPropertiesServiceTests
+    {
+        [Test]
+        public void ShouldSetContentTypeWhenNServiceBusHeaderExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.ContentType.Should().Be("application/json");
+        }
+
+        [Test]
+        public void ShouldNotSetContentTypeWhenNServiceBusHeaderDoesNotExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+            rawMessage.Headers.Remove("NServiceBus.ContentType");
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.ContentType.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldSetMessageIdWhenNServiceBusHeaderExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.MessageId.Should().Be("695742b4-58d0-4e3a-83a9-a4120116c48d");
+        }
+
+        [Test]
+        public void ShouldNotSetMessageIdWhenNServiceBusHeaderDoesNotExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+            rawMessage.Headers.Remove("NServiceBus.MessageId");
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.MessageId.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldSetCorrelationIdWhenNServiceBusHeaderExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.CorrelationId.Should().Be("695742b4-58d0-4e3a-83a9-a4120116c48d");
+        }
+
+        [Test]
+        public void ShouldNotSetCorrelationIdWhenNServiceBusHeaderDoesNotExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+            rawMessage.Headers.Remove("NServiceBus.CorrelationId");
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.CorrelationId.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldSetTypeWhenNServiceBusHeaderExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.Type.Should().Be("Autobahn.Configurations.Contracts.Commands.ValidateConfigurations");
+        }
+
+        [Test]
+        public void ShouldNotSetTypeWhenNServiceBusHeaderDoesNotExists()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+            rawMessage.Headers.Remove("NServiceBus.EnclosedMessageTypes");
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.Type.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldNotSetDeliveryModeToPersistent()
+        {
+            //arrange
+            var rawMessage = GetRawMessageForTest();
+            byte persistentDeliverMode = 2;
+
+            var service = new CreateBasicPropertiesService();
+
+            //act
+            var properties = service.Create(rawMessage);
+
+            //assert
+            properties.DeliveryMode.Should().Be(persistentDeliverMode);
+        }
+
+
+        private static RawMessage GetRawMessageForTest()
+        {
+            string data;
+            using (var reader = new StreamReader(Path.Combine("../../TestData", "Error.json")))
+            {
+                data = reader.ReadToEnd();
+            }
+            var rawMessage = JsonConvert.DeserializeObject<RawMessage>(data);
+            return rawMessage;
+        }
+
+
+    }
+}
