9619ca2d18b98cfa326f8c730251ba30ae380ae7
Tom Cabanski <tom@cabanski.com>
2016-01-02 11:51:43 -0600

    Get rid of obsolete settings tests around message handling rules

--- test/RabbitOperations.Collector.Tests.Unit/Configuration/SettingsTests.cs
+++ test/RabbitOperations.Collector.Tests.Unit/Configuration/SettingsTests.cs
@@ -10,6 +10,7 @@ using RabbitOperations.Collector.Configuration;
 using Microsoft.Extensions.OptionsModel;
 using Moq;
 using RabbitOperations.Collector.Tests.Unit.RavenDb;
+using FluentAssertions;
 
 namespace RabbitOperations.Collector.Tests.Unit.Configuration
 {
@@ -27,101 +28,26 @@ namespace RabbitOperations.Collector.Tests.Unit.Configuration
         public void SettingsCanBeSavedAndLoaded()
         {
             //arrange
-            var mockSettings = Mock.Of<IOptions<AppSettings>>();
-            var settings = new Settings(ravenDb.Store, mockSettings);
+            Settings settings = CreateSettings();
             const string applicationId = "one";
-            //settings.Applications.Add(new ApplicationConfiguration
-            //{
-            //    AuditQueue = "xxx",
-            //    ApplicationId = applicationId
-            //});
-            //settings.Save();
+            settings.Applications.Add(new ApplicationConfiguration
+            {
+                AuditQueue = "xxx",
+                ApplicationId = applicationId
+            });
+            settings.Save();
 
-            ////act
-            //settings = new Settings(Store);
+            //act
+            settings = CreateSettings();
 
-            ////assert 
-            //settings.Applications.First(x => x.ApplicationId == applicationId).AuditQueue.Should().Be("xxx");
+            //assert 
+            settings.Applications.First(x => x.ApplicationId == applicationId).AuditQueue.Should().Be("xxx");
         }
 
-        //[Test]
-        //public void CanGetMessageTypeHandlerForATypeWhenOneMatches()
-        //{
-        //    //arrange
-        //    var settings = new Settings(Store);
-        //    settings.GlobalMessageHandlingInstructions = new List<MessageTypeHandling>
-        //    {
-        //        new MessageTypeHandling
-        //        {
-        //            MessageTypes = new List<string>
-        //            {
-        //                "a",
-        //                "b"
-        //            },
-        //            Keywords = new List<string>
-        //            {
-        //                "one"
-        //            }
-        //        },
-        //        new MessageTypeHandling
-        //        {
-        //            MessageTypes = new List<string>
-        //            {
-        //                "c",
-        //                "d"
-        //            },
-        //            Keywords = new List<string>
-        //            {
-        //                "two"
-        //            }
-        //        }
-        //    };
-
-        //    //act
-        //    var handling = settings.MessageTypeHandlingFor("c");
-
-        //    //assert
-        //    handling.Keywords[0].Should().Be("two");
-        //}
-
-        //[Test]
-        //public void ShouldReturnFirstHandlerThatHasMatchingTypeWhenMultiplesMatch()
-        //{
-        //    //arrange
-        //    var settings = new Settings(Store);
-        //    settings.GlobalMessageHandlingInstructions = new List<MessageTypeHandling>
-        //    {
-        //        new MessageTypeHandling
-        //        {
-        //            MessageTypes = new List<string>
-        //            {
-        //                "a",
-        //                "c"
-        //            },
-        //            Keywords = new List<string>
-        //            {
-        //                "one"
-        //            }
-        //        },
-        //        new MessageTypeHandling
-        //        {
-        //            MessageTypes = new List<string>
-        //            {
-        //                "c",
-        //                "d"
-        //            },
-        //            Keywords = new List<string>
-        //            {
-        //                "two"
-        //            }
-        //        }
-        //    };
-
-        //    //act
-        //    var handling = settings.MessageTypeHandlingFor("c");
-
-        //    //assert
-        //    handling.Keywords[0].Should().Be("one");
-        //}
+        private Settings CreateSettings()
+        {
+            var mockSettings = Mock.Of<IOptions<AppSettings>>();
+            return new Settings(ravenDb.Store, mockSettings);
+        }
     }
 }
