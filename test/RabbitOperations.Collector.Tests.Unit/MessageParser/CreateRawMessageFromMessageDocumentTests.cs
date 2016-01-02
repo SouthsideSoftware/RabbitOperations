b6a41cd3f38d2e90a63a42727883bca15e4e1371
Tom Cabanski <tom@cabanski.com>
2016-01-02 15:13:35 -0600

    Tests for creating a raw message

---
+++ test/RabbitOperations.Collector.Tests.Unit/MessageParser/CreateRawMessageFromMessageDocumentTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using RabbitOperations.Domain;
+using Xunit;
+
+namespace RabbitOperations.Collector.Tests.Unit.MessageParser
+{
+    public class CreateRawMessageFromMessageDocumentTests
+    {
+        [Fact]
+        public void CanProperlyCopyBodyFromDocument()
+        {
+            //arrange
+            var messageDocument = new MessageDocument
+            {
+                Body = "This is a test"
+            };
+
+            //act
+            var rawMessage = new RawMessage(messageDocument);
+
+            //assert
+            rawMessage.Body.Should().Be(messageDocument.Body);
+        }
+
+        [Fact]
+        public void CanProperlyCopyHeadersFromDocument()
+        {
+            //arrange
+            var messageDocument = new MessageDocument
+            {
+                Headers = new Dictionary<string, string>
+                {
+                    {"one", "oneValue"},
+                    {"two", "twoValue"}
+                },
+                Body = ""
+            };
+
+            //act
+            var rawMessage = new RawMessage(messageDocument);
+
+            //assert
+            rawMessage.Headers.ShouldBeEquivalentTo(messageDocument.Headers);
+        }
+    }
+}
