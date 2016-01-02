b55e05769a4019c6b16e22f0cc423213fd976560
Tom Cabanski <tom@cabanski.com>
2016-01-02 15:19:10 -0600

    All existing message parser tests moved over

---
+++ test/RabbitOperations.Collector.Tests.Unit/MessageParser/RawMessageTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Framing;
+using RabbitOperations.Domain;
+using Xunit;
+
+namespace RabbitOperations.Collector.Tests.Unit.MessageParser
+{
+    public class RawMessageTests
+    {
+        [Fact]
+        public void CanReadAudit()
+        {
+            //arrange
+            string data;
+            using (var reader = new StreamReader(Path.Combine("./TestData", "Audit.json")))
+            {
+                data = reader.ReadToEnd();
+            }
+
+            //act
+            var rawMessage = JsonConvert.DeserializeObject<RawMessage>(data);
+
+            //assert
+            rawMessage.Headers.Count.Should().BeGreaterThan(1);
+        }
+
+        [Fact]
+        public void CanReadError()
+        {
+            //arrange
+            string data;
+            using (var reader = new StreamReader(Path.Combine("./TestData", "Error.json")))
+            {
+                data = reader.ReadToEnd();
+            }
+
+            //act
+            var rawMessage = JsonConvert.DeserializeObject<RawMessage>(data);
+
+            //assert
+            rawMessage.Headers.Count.Should().BeGreaterThan(1);
+        }
+
+        [Fact]
+        public void CanRoundTripBody()
+        {
+            //arrange
+            var rawMessage = MessageTestHelpers.GetAuditMessage();
+
+            //act
+            var publishData = rawMessage.GetEelementsForRabbitPublish();
+            var newRawMessage =
+                new RawMessage(new BasicDeliverEventArgs("tag", 1, false, "exchange", "",
+                    new BasicProperties {Headers = publishData.Item2}, publishData.Item1));
+
+            //assert
+            newRawMessage.Body.Should().Be(rawMessage.Body);
+        }
+
+        [Fact]
+        public void CanRoundTripHeaders()
+        {
+            //arrange
+            var rawMessage = MessageTestHelpers.GetAuditMessage();
+
+            //act
+            var publishData = rawMessage.GetEelementsForRabbitPublish();
+            var newRawMessage =
+                new RawMessage(new BasicDeliverEventArgs("tag", 1, false, "exchange", "",
+                    new BasicProperties {Headers = publishData.Item2}, publishData.Item1));
+
+            //assert
+            newRawMessage.Headers.ShouldBeEquivalentTo(rawMessage.Headers);
+        }
+
+        [Fact]
+        public void HeadersAreDeepCopiedWhenConstructedFromMessageDocument()
+        {
+            //arrange
+            var doc = new MessageDocument();
+            var originalHeaders = new Dictionary<string, string>
+            {
+                {"foo1", "foo1"},
+                {"foo2", "foo2"}
+            };
+
+            doc.Headers = originalHeaders;
+
+            //act
+            var rawMessage = new RawMessage(doc);
+            rawMessage.Headers["foo1"] = "fi1";
+            rawMessage.Headers.Remove("foo2");
+
+            //assert
+            rawMessage.Headers.Should()
+                .NotBeSameAs(originalHeaders,
+                    "The raw message dictionary is not the same as the message document dictionary");
+            doc.Headers.Should().Equal(originalHeaders, "The original message's headers are unchanged");
+        }
+    }
+}
---
+++ test/RabbitOperations.Collector.Tests.Unit/MessageParser/TypeNameTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using FluentAssertions;
+using RabbitOperations.Domain;
+using Xunit;
+
+namespace RabbitOperations.Collector.Tests.Unit.MessageParser
+{
+    public class TypeNameTests
+    {
+        [Fact]
+        public void CanConstructFromFullName()
+        {
+            //arrange and act
+            var typeName =
+                new TypeName(
+                    "Autobahn.Fulfillment.Contracts.Ordering.NotifyOrderHasBeenCanceled, Autobahn.Fulfillment.Contracts, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+
+            //assert
+            typeName.Should().Be(new TypeName
+            {
+                Assembly = "Autobahn.Fulfillment.Contracts",
+                ClassName = "NotifyOrderHasBeenCanceled",
+                Culture = "neutral",
+                Namespace = "Autobahn.Fulfillment.Contracts.Ordering",
+                PublicKeyToken = "null",
+                Version = new Version(1, 0, 0, 0)
+            });
+        }
+
+        [Fact]
+        public void CanConstructFromFullNameWhenClassNameHasNoNamespace()
+        {
+            //arrange and act
+            var typeName =
+                new TypeName(
+                    "NotifyOrderHasBeenCanceled, Autobahn.Fulfillment.Contracts, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+
+            //assert
+            typeName.Should().Be(new TypeName
+            {
+                Assembly = "Autobahn.Fulfillment.Contracts",
+                ClassName = "NotifyOrderHasBeenCanceled",
+                Culture = "neutral",
+                Namespace = "",
+                PublicKeyToken = "null",
+                Version = new Version(1, 0, 0, 0)
+            });
+        }
+
+        [Fact]
+        public void ShouldReturnAFullClassStringFromToString()
+        {
+            //arrange and act
+            var fullName =
+                "Autobahn.Fulfillment.Contracts.Ordering.NotifyOrderHasBeenCanceled, Autobahn.Fulfillment.Contracts, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+            var typeName =
+                new TypeName(
+                    fullName);
+
+            //assert
+            typeName.ToString().Should().Be(fullName);
+        }
+    }
+}
