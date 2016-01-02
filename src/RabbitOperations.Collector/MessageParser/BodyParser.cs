21b5790e2a87b143c916f9f2a83061ae054e4bb0
Tom Cabanski <tom@cabanski.com>
2016-01-02 12:09:03 -0600

    Bring back the body parser and related tests

---
+++ src/RabbitOperations.Collector/MessageParser/BodyParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RabbitOperations.Collector.Configuration.Interfaces;
+using RabbitOperations.Collector.MessageParser.Interfaces;
+using RabbitOperations.Domain;
+using SouthsideUtility.Core.DesignByContract;
+
+namespace RabbitOperations.Collector.MessageParser
+{
+    public class BodyParser : IBodyParser
+    {
+        private readonly ISettings settings;
+
+        public BodyParser(ISettings settings)
+        {
+            Verify.RequireNotNull(settings, "settings");
+
+            this.settings = settings;
+        }
+
+        public void ParseBody(IRawMessage message, MessageDocument messageDocument)
+        {
+            messageDocument.Body = message.Body;
+        }
+    }
+}
---
+++ test/RabbitOperations.Collector.Tests.Unit/MessageParser/BodyParserTests.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RabbitOperations.Domain;
+using Xunit;
+using Autofac.Extras.Moq;
+using RabbitOperations.Collector.MessageParser;
+
+namespace RabbitOperations.Collector.Tests.Unit.MessageParser
+{
+    public class BodyParserTests
+    {
+        private const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ss:ffffffZ";
+        private const string ApplicationJsonContentType = "application/json";
+
+        [Fact]
+        public void BodyParserCapturesRawBodyFromAudit()
+        {
+            //arrange
+            string data;
+            using (var reader = new StreamReader(Path.Combine("./TestData", "Audit.json")))
+            {
+                data = reader.ReadToEnd();
+            }
+            var rawMessage = JsonConvert.DeserializeObject<RawMessage>(data);
+            var mocker = AutoMock.GetLoose();
+            var bodyParser = mocker.Create<BodyParser>();
+            var doc = new MessageDocument();
+
+            //act
+            bodyParser.ParseBody(rawMessage, doc);
+
+            //assert
+            doc.Body.Should().Be("{\"CorrelationGuid\":\"f349702d-1be6-4c65-8f74-de8457ed4ccf\"}");
+        }
+
+        [Fact]
+        public void BodyParserCapturesRawBodyFromError()
+        {
+            //arrange
+            string data;
+            using (var reader = new StreamReader(Path.Combine("./TestData", "Error.json")))
+            {
+                data = reader.ReadToEnd();
+            }
+            var rawMessage = JsonConvert.DeserializeObject<RawMessage>(data);
+            var mocker = AutoMock.GetLoose();
+            var bodyParser = mocker.Create<BodyParser>();
+            var doc = new MessageDocument();
+
+            //act
+            bodyParser.ParseBody(rawMessage, doc);
+
+            //assert
+            doc.Body.Should().Be("{\"Ids\":[\"afecc831-34d4-47ca-b43b-56eb90d4e3b6\"]}");
+        }
+
+        [Fact]
+        public void BodyParserHandlesMessagesWithEmptyBodies()
+        {
+            //arrange
+            string data;
+            using (var reader = new StreamReader(Path.Combine("./TestData", "EmptyBody.json")))
+            {
+                data = reader.ReadToEnd();
+            }
+            var mocker = AutoMock.GetLoose();
+            var rawMessage = JsonConvert.DeserializeObject<RawMessage>(data);
+            var bodyParser = mocker.Create<BodyParser>();
+            var doc = new MessageDocument();
+
+            //act
+            bodyParser.ParseBody(rawMessage, doc);
+
+            //assert
+            doc.Body.Should().BeEmpty();
+        }
+    }
+}
