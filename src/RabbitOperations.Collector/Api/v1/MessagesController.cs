17e4cdc7cb930e481230714e4ea7f660ca0f3945
Tom Cabanski <tom@cabanski.com>
2016-01-16 09:27:39 -0600

    Upgrade some of the test libs. Add missing files from search service

---
+++ src/RabbitOperations.Collector/Api/v1/MessagesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using RabbitOperations.Collector.Models;
+using RabbitOperations.Collector.RavenDb.Query;
+using RabbitOperations.Collector.RavenDb.Query.Interfaces;
+using RabbitOperations.Domain;
+using SouthsideUtility.Core.DesignByContract;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RabbitOperations.Collector.Api.v1
+{
+    [Route("api/[controller]", Name = "defaultMessages")]
+    [Route("api/v1/[controller]", Name = "version1Messages")]
+    public class MessagesController : Controller
+    {
+        private readonly IBasicSearch basicSearch;
+
+        public MessagesController(IBasicSearch basicSearch)
+        {
+            Verify.RequireNotNull(basicSearch, "basicSearch");
+
+            this.basicSearch = basicSearch;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public SearchResult<MessageSearchResult> Get(SearchModel searchModel)
+        {
+            Verify.RequireNotNull(searchModel, "searchModel");
+
+            return basicSearch.Search(searchModel);
+        }
+    }
+}
---
+++ src/RabbitOperations.Collector/RavenDb/Query/QueryModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using RabbitOperations.Collector.RavenDb.Query.Interfaces;
+
+namespace RabbitOperations.Collector.RavenDb.Query
+{
+    public class QueryModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BasicSearch>().As<IBasicSearch>().SingleInstance();
+        }
+    }
+}
