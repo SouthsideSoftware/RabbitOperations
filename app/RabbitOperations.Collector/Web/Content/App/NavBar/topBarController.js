1da85095c190b9648b73e7949ffb38539e6d79cb
Tom Cabanski <tom@cabanski.com>
2016-07-16 09:46:21 -0500

    Alsio have to update URL of notes for cache bust

--- app/RabbitOperations.Collector/Web/Content/App/NavBar/topBarController.js
+++ app/RabbitOperations.Collector/Web/Content/App/NavBar/topBarController.js
@@ -1,7 +1,7 @@
 ﻿rabbitOperationsApp.controller('topBarController', function ($scope, $modal) {
     $scope.showWhatsNew = function () {
         var modalInstance = $modal.open({
-            templateUrl: '/Content/App/Dashboard/Popups/whatsNew.html?version=1',
+            templateUrl: '/Content/App/Dashboard/Popups/whatsNew.html?version=2',
             controller: 'whatsNewController',
             size: 'lg'
         });
