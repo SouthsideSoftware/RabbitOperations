dd40bbc21347bfd4eceffe9f592cb344c2786eb6
Tom Cabanski <tom@cabanski.com>
2016-07-21 06:09:25 -0500

    Pull back to latest Raven 3.0 (includes patches)

--- app/RabbitOperations.Collector/Web/Content/App/NavBar/topBarController.js
+++ app/RabbitOperations.Collector/Web/Content/App/NavBar/topBarController.js
@@ -1,7 +1,7 @@
 ﻿rabbitOperationsApp.controller('topBarController', function ($scope, $modal) {
     $scope.showWhatsNew = function () {
         var modalInstance = $modal.open({
-            templateUrl: '/Content/App/Dashboard/Popups/whatsNew.html?version=6',
+            templateUrl: '/Content/App/Dashboard/Popups/whatsNew.html?version=7',
             controller: 'whatsNewController',
             size: 'lg'
         });
