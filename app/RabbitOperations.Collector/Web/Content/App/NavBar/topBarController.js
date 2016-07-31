e65e367cfc489b672f886dec89b48a9c7ecfffb0
Tom Cabanski <tom@cabanski.com>
2016-07-31 08:24:56 -0500

    Update release notes.

--- app/RabbitOperations.Collector/Web/Content/App/NavBar/topBarController.js
+++ app/RabbitOperations.Collector/Web/Content/App/NavBar/topBarController.js
@@ -1,7 +1,7 @@
 ﻿rabbitOperationsApp.controller('topBarController', function ($scope, $modal) {
     $scope.showWhatsNew = function () {
         var modalInstance = $modal.open({
-            templateUrl: '/Content/App/Dashboard/Popups/whatsNew.html?version=10',
+            templateUrl: '/Content/App/Dashboard/Popups/whatsNew.html?version=11',
             controller: 'whatsNewController',
             size: 'lg'
         });
